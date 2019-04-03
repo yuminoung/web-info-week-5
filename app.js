931862fbcf39b462b99376c1f25250fc1695b2e0
yuminoung <yuminoung.dev@gmail.com>
2019-04-03 17:36:10 +1100

    remove modules

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => { send('Hello') })
 
 PORT_NUMBER = 3000 || process.env.PORT_NUMBER
 app.listen(PORT_NUMBER, () => console.log('Listening...'))
