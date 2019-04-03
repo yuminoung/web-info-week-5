67366010836c30d8fe3b73d59d2c52f13ee3274f
yuminoung <yuminoung.dev@gmail.com>
2019-04-03 18:00:14 +1100

    test

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello Commit'))
 
-PORT_NUMBER = 3000 || process.env.PORT_NUMBER
+PORT_NUMBER = 3000 || process.env.PORT
 app.listen(PORT_NUMBER, () => console.log('Listening...'))
